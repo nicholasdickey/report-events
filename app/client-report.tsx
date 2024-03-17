d38c1aa7d72257cffa2fe6bf8005f16e8c1fbd9f
nicholas.dickey <nicholas.dickey@hudsonwilde.com>
2024-03-17 15:05:34 -0400

    Add min to eports

--- app/client-report.tsx
+++ app/client-report.tsx
@@ -10,9 +10,10 @@ export default function Home() {
     const [expanded, setExpanded] = useState<string>("");
     const [target, setTarget] = useState<string>("qwiket");
     const [bot, setBot] = useState<boolean>(false);
+    const [min, setMin] = useState<number>(0);
     console.log("bot=", bot)
     const fetchReportKey = (pageIndex: number, previousPageData: any): FetchReportKey | null => {
-        let key: FetchReportKey = { target, page: pageIndex, bot };
+        let key: FetchReportKey = { target, page: pageIndex, bot,min };
         if (previousPageData && !previousPageData.length) return null // reached the end
         return key;
     }
@@ -52,8 +53,12 @@ export default function Home() {
 
                         </select>
                         <div className="flex items-center mb-4 mt-4">
-                            <input onChange={(event) => { console.log("setBot=", event.target.checked); setBot(event.target.checked ? true : false) }} id="disabled-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <input onChange={(event) => { console.log("setBot=", event.target.checked); setBot(event.target.checked ? true : false) }} id="include-bots" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                             <label htmlFor="disabled-checkbox" className="ms-2 text-sm font-medium text-gray-400 dark:text-gray-500">Include Bots</label>
+                        </div>
+                        <div className="flex items-center mb-4 mt-4">
+                            <input onChange={(event) => { console.log("setFilter=", event.target.value); setMin(+event.target.value) }} id="min-events" type="text" value={min} className="w-12 h-7 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label htmlFor="disabled-checkbox" className="ms-2 text-sm font-medium text-gray-400 dark:text-gray-500">Minimum Events</label>
                         </div>
                     </form>
                     <div className="clear-both"></div>
--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,17 @@
 // SWR infinite:
 // SWR get stories and mentions grouped by story
 // 
-export type FetchReportKey = { target: string, page: number,bot:boolean };
-export const fetchReport = async ({ target, page,bot }: FetchReportKey) => {
+export type FetchReportKey = { target: string, page: number,bot:boolean,min:number };
+export const fetchReport = async ({ target, page,bot,min }: FetchReportKey) => {
   try {
 
     console.log("api: fetchReport", target, page)
     let url = '';
     if(target=='qwiket')
-        url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/events/report?page=${page || 0}&bot=${bot?1:0}`;
+        url = `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/findexar/events/report?page=${page || 0}&bot=${bot?1:0}&min=${min}`;
     else
-        url =  `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/prayer/report?page=${page||0}&bot=${bot?1:0}`;
+        url =  `${process.env.NEXT_PUBLIC_LAKEAPI}/api/v41/prayer/report?page=${page||0}&bot=${bot?1:0}&min=${min}`;
+    
     console.log("fetchReport-url", url)
     // const res = await axios.get(url);
     const res = await fetch(url);
